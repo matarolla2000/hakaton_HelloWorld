64cff270dee499106d74bb174eb73d988e644649
matarolla2000 <matarolla2000@gmail.com>
2026-07-10 12:59:32 +0500

    apdated app.py, login.html, main.html

--- app.py
+++ app.py
@@ -1,24 +1,37 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, session
 
 app = Flask(__name__)
-
-
+app.secret_key = 'any_secret_string_here'
 logined = False
+
 @app.route("/")
 def index():
     return redirect(url_for("main"))
 
 @app.route("/main")
 def main():
-    if logined == True:
-        return render_template("main.html")
+    if not session.get('logined', False):
+        return redirect(url_for('login'))
     else:
-        return login()
+        return render_template("main.html", username=session.get("username"))
     
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        username = request.form.get('login')
+        password = request.form.get('password')
+        session['logined'] = True
+        session['username'] = username
+        return redirect(url_for("main"))
     return render_template("login.html")
 
 @app.route("/test")
 def test():
     return render_template("test.html")
+
+@app.route("/logout", methods=["POST"])
+def logout():
+    session.clear
+    return redirect(url_for("login"))
+
+app.run(debug=True)
